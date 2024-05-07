12d255571af0e23d587654641c2920e1b1044000
takupasmeong <57649441+takupasmeong@users.noreply.github.com>
2024-05-07 14:50:59 +0700

    Move the game from console to page

--- script.js
+++ script.js
@@ -10,30 +10,86 @@ function playRound(playerSelection, computerSelection) {
     let toNum = (playerSelection === 'rock') ? 0 :
                 (playerSelection === 'paper') ? 1 :
                 (playerSelection === 'scissors') ? 2 : null;
+    let playerScore = 0;
+    let compScore = 0;
 
     let toString = (computerSelection == 0) ? "rock" : 
                 (computerSelection == 1) ? "paper" : 
                 (computerSelection == 2) ? "scissors": "wrong number";
-    
-    let result = (toNum == computerSelection) ? "draw" :
-                (toNum == 0 && computerSelection == 1) ? `You lose! ${toString} beats ${playerSelection}` :
-                (toNum == 0 && computerSelection == 2) ? `You win! ${playerSelection} beats ${toString}` :
-                (toNum == 1 && computerSelection == 0) ? `You lose! ${toString} beats ${playerSelection}` :
-                (toNum == 1 && computerSelection == 2) ? `You win! ${playerSelection} beats ${toString}` :
-                (toNum == 2 && computerSelection == 0) ? `You lose! ${toString} beats ${playerSelection}` :
-                (toNum == 2 && computerSelection == 1) ? `You win! ${playerSelection} beats ${toString}` :
-                "Your input is wrong!";
 
-    return result;
-}
-
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Input");
-        let computerSelection = getComputerChoice();
-        console.log(computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
+    if (toNum == computerSelection) {
+        result = "draw";
+    } else if (toNum == 0 && computerSelection == 1) {
+        result = `You lose! ${toString} beats ${playerSelection}`;
+        compScore++;
+    } else if (toNum == 0 && computerSelection == 2) {
+        result = `You win! ${playerSelection} beats ${toString}`;
+        playerScore++;
+    } else if (toNum == 1 && computerSelection == 0) {
+        result = `You lose! ${toString} beats ${playerSelection}`;
+        compScore++;
+    } else if (toNum == 1 && computerSelection == 2) {
+        result = `You win! ${playerSelection} beats ${toString}`
+        playerScore++;
+    } else if (toNum == 2 && computerSelection == 0) {
+        result = `You lose! ${toString} beats ${playerSelection}`;
+        compScore++;
+    } else if (toNum == 2 && computerSelection == 1) {
+        result = `You win! ${playerSelection} beats ${toString}`;
+        playerScore++;
+    } else {
+        result = "Your input is wrong!";
     }
+
+    return {
+        result: result,
+        playerScore: playerScore,
+        compScore: compScore
+    };
 }
 
-playGame();
+// function playGame() {
+//     for (let i = 0; i < 5; i++) {
+//         let playerSelection = prompt("Input");
+//         let computerSelection = getComputerChoice();
+//         console.log(computerSelection);
+//         console.log(playRound(playerSelection, computerSelection));
+//     }
+// }
+
+const menu = document.querySelector("#menu");
+const output = document.querySelector("#output");
+const scoreboard = document.querySelector("#scoreboard");
+
+let playerScore = 0;
+let compScore = 0;
+
+menu.addEventListener("click", (event) => {
+    const target = event.target;
+
+    let playerSelection = target.id;
+    let computerSelection = getComputerChoice();
+
+    const gameValues = playRound(playerSelection, computerSelection);
+
+    output.textContent = gameValues.result;
+    if (gameValues.playerScore == 1) {
+        playerScore++;
+        updateScore();
+    }
+    if (gameValues.compScore == 1) {
+        compScore++;
+        updateScore();
+    }
+
+    if (playerScore == 5) {
+        alert("Game is finish. You Win!!!")
+    }
+    if (compScore == 5) {
+        alert("Game is finish. You lose!!!");
+    }
+});
+
+function updateScore() {
+    scoreboard.textContent = `Player:${playerScore} Computer:${compScore}`;
+}
